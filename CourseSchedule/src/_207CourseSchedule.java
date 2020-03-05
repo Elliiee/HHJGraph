fc05061b03337fe58c72f4122030cb0119195b78
Elliiee <danyiluozhang@gmail.com>
2020-03-05 11:41:07 -0600

    207

--- CourseSchedule/src/_207CourseSchedule.java
+++ CourseSchedule/src/_207CourseSchedule.java
@@ -10,6 +10,73 @@ class GNode{
 }
 
 public class _207CourseSchedule {
+    /*
+    Approach 1: Backtracking
+    The problem could be modeled as a graph traversal problem.
+    Each course can be represented as a vertex.
+    The dependency between the courses can be modeled as a directed edge between 2 vertexes.
+
+    If the student can finish all the courses, means there is no cycle existed in the Graph.
+    That is called DAG (Directed Acyclic Graph).
+
+    Backtracking is a general algorithm that is often applied to solve the constraint
+    satisfaction problems, which incrementally builds candidates to the solutions, and
+    abandons a candidate / aka: backtracks as soon as it determines that the candidate
+    would not lead to a valid solution.
+
+    Enumerate each vertex to check if it could for a cyclic path. If so, return false as a result.
+     */
+    public boolean canFinishBacktracking(int numCourses, int[][] prerequisites){
+        // course -> list of next courses
+        HashMap<Integer, List<Integer>> courseDict = new HashMap<>();
+
+        // build the graph first
+        for (int[] relation : prerequisites){
+            // relation[0] depends on relation[1]
+            if (courseDict.containsKey(relation[1])){
+                courseDict.get(relation[1]).add(relation[0]);
+            } else {
+                List<Integer> nextCourses = new LinkedList<>();
+                nextCourses.add(relation[0]);
+                courseDict.put(relation[1], nextCourses);
+            }
+        }
+
+        boolean[] path = new boolean[numCourses];
+
+        for (int currCourse = 0; currCourse < numCourses; currCourse++){
+            if (this.isCyclic(currCourse, courseDict, path))
+                return false;
+        }
+
+        return true;
+    }
+
+    /*
+    Backtracking to check if there is cyclic
+     */
+    protected boolean isCyclic(Integer currCourse, HashMap<Integer, List<Integer>> courseDict,
+                               boolean[] path){
+        if (path[currCourse]) // already in the path, loop
+            return true;
+
+        if (!courseDict.containsKey(currCourse)) // no following courses, no loop
+            return false;
+
+        path[currCourse] = true; // mark in the path before backtracking
+
+        // backtracking
+        boolean ret = false;
+        for (Integer nextCourse : courseDict.get(currCourse)){
+            ret = this.isCyclic(nextCourse, courseDict, path);
+            if (ret)
+                break;
+        }
+
+        path[currCourse] = false; // remove the node from the path after backtracking.
+        return ret;
+    }
+
     public boolean canFinish(int numCourses, int[][] prerequisites){
         /*
         Approach 3: topological sort
---
+++ KeysAndRooms/src/_1202SmallestStringWithSwaps.java
@@ -0,0 +1,9 @@
+public class _1202SmallestStringWithSwaps {
+
+}
+/*
+You are given a string s.
+And given an array of pairs of indices in the string.
+You can swap the characters at any pair of indices in the given pairs and any number of times.
+Return the lexicographically smallest string that s can be changed to after using the swaps.
+ */
